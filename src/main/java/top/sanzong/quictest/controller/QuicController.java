4db5786067f8f08e4d02b339930c8c6e37e37a16
SANZONG <2577786020@qq.com>
2022-05-15 20:42:57 +0800

    nothing

--- src/main/java/top/sanzong/quictest/controller/QuicController.java
+++ src/main/java/top/sanzong/quictest/controller/QuicController.java
@@ -2,11 +2,13 @@ package top.sanzong.quictest.controller;
 
 
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
  * @author SANZONG
  */
+@RequestMapping("/api")
 @RestController
 public class QuicController {
     @PostMapping("/quic-test")
